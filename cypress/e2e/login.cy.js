88177f9eb1bf6384eb63e2cf35bab3ae482bf568
Ramzesito <roman_shv@mail.ru>
2023-12-03 17:33:46 +0300

    task 2 begins

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,10 +2,13 @@ describe("Book all tests", () => {
 
     beforeEach(() => {
         cy.visit("/");
+        let resX = Cypress.env('note').x;
+        let resY = Cypress.env('note').y;
+        cy.viewport(resX, resY);
     });
 
-    it("Should successfully login", () => {
-        cy.login("test@test.com", "test");
+    it.only("Should successfully login", () => {    
+        cy.login(Cypress.env('note').x, "test");
         cy.contains("Добро пожаловать test@test.com").should("be.visible");
     });
 
@@ -26,5 +29,14 @@ describe("Book all tests", () => {
         cy.get('#pass').then($el => $el[0].checkValidity())
             .should('be.false');
     });
+
+    // it.only("Should add existing book in favorites", () => {
+    //     cy.login("test@test.com", "test");
+        
+    //     let t = cy.get(".mt-3 .card-title").contains("Book1").get(".btn-success").click();
+    //     cy.log(t);
+        
+    //     //cy.get(".mt-3 .card-title")
+    // });
 
 });
