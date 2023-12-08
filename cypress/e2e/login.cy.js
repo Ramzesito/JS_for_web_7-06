2b3d54c2cd8abecf6726694f31b3f366ac221807
Ramzesito <roman_shv@mail.ru>
2023-12-08 16:47:32 +0300

    3 tests added

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -17,8 +17,7 @@ describe("Book all tests", () => {
             .should("be.false");
         cy.get("#mail")
             .then(($el) => $el[0].validationMessage)
-            .should("contain", "Заполните это поле.");
-            
+            .should("contain", "Заполните это поле.");            
     });
 
     it("Should not login with empty password", () => {
@@ -27,4 +26,28 @@ describe("Book all tests", () => {
             .should('be.false');
     });
 
+    // первоначальное условие - книга еще не должна быть в избранном
+    it("Should add to favorites", () => {
+        cy.login("test@test.com", "test");
+        cy.get("a.mt-3 .card-title").contains("title1").parent().parent().find(".btn").click();    
+    });
+
+    // первоначальное условие - книга уже должна быть в избранном
+    it("Should delete from favorites", () => {
+        cy.login("test@test.com", "test");
+        cy.get("a.mt-3 .card-title").contains("title1").parent().parent().find(".btn").click();    
+    });
+
+    // перейти в Favorites и убедиться, что находимся там (при наличии хоть одной книги)
+    it.only("Should be in favorites", () => {
+        cy.login("test@test.com", "test");
+        cy.visit("/favorites");
+        let favBooks = cy.get("a.mt-3 .card-title").length;    
+        //favBooks.forEach((book) => 
+        //    cy.log(book));
+        cy.log(favBooks);
+        cy.log("sss");
+    });
+
 });
+
